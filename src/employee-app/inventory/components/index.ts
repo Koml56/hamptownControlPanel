6b22943485b76e52c90a89468f430555981a526d
Koml56 <122701871+Koml56@users.noreply.github.com>
2025-07-30 02:39:29 +0300

    Create index.ts

---
+++ src/employee-app/inventory/components/index.ts
@@ -0,0 +1,17 @@
+// src/employee-app/inventory/components/index.ts
+export { default as InventoryHeader } from './InventoryHeader';
+export { default as TabNavigation } from './TabNavigation';
+export { default as ItemCard } from './ItemCard';
+export { default as DatabaseView } from './DatabaseView';
+export { default as DailyView } from './DailyView';
+export { default as WeeklyView } from './WeeklyView';
+export { default as MonthlyView } from './MonthlyView';
+export { default as ReportsView } from './ReportsView';
+export { default as ImportModal } from './ImportModal';
+export { default as ManualItemModal } from './ManualItemModal';
+export { default as CategoryModal } from './CategoryModal';
+export { default as CountModal } from './CountModal';
+export { default as WasteModal } from './WasteModal';
+export { default as BulkActionsBar } from './BulkActionsBar';
+export { default as ScrollPicker } from './ScrollPicker';
+export { default as ToastContainer } from './ToastContainer';
