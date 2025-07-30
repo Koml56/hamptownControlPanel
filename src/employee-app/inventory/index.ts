386e73a67231d07c3b6d6a2caa33047c9b57c85d
Koml56 <122701871+Koml56@users.noreply.github.com>
2025-07-30 03:16:36 +0300

    Update index.ts

--- src/employee-app/inventory/index.ts
+++ src/employee-app/inventory/index.ts
@@ -1,22 +1,19 @@
 // src/employee-app/inventory/components/index.ts
-// Only export components that actually exist
+// Export all inventory components
 
+export { default as InventoryHeader } from './InventoryHeader';
+export { default as TabNavigation } from './TabNavigation';
+export { default as ItemCard } from './ItemCard';
 export { default as DatabaseView } from './DatabaseView';
 export { default as DailyView } from './DailyView';
 export { default as WeeklyView } from './WeeklyView';
 export { default as MonthlyView } from './MonthlyView';
 export { default as ReportsView } from './ReportsView';
-
-export { default as ItemCard } from './ItemCard';
-export { default as ToastContainer } from './ToastContainer';
-export { default as BulkActionsBar } from './BulkActionsBar';
-
 export { default as ImportModal } from './ImportModal';
 export { default as ManualItemModal } from './ManualItemModal';
 export { default as CategoryModal } from './CategoryModal';
 export { default as CountModal } from './CountModal';
 export { default as WasteModal } from './WasteModal';
+export { default as BulkActionsBar } from './BulkActionsBar';
 export { default as ScrollPicker } from './ScrollPicker';
-
-// Note: InventoryHeader and TabNavigation are not separate files
-// They are integrated into the main RestaurantInventory component
+export { default as ToastContainer } from './ToastContainer';
